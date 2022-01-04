da262c287812adc16a92155449a2868be274c5e5
Marcelo Marçal <marcelo.a.marcal@hotmail.com>
2022-01-04 14:38:42 -0300

    Desafio 02 Concluído

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,23 @@
+import { useMovies } from "../MoviesContext";
+import { MovieCard } from "./MovieCard";
+
 export function Content() {
-  // Complete aqui
+
+  const {selectedGenre, movies} = useMovies();
+
+  return (
+    <div className="container">
+        <header>
+          <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+        </header>
+
+        <main>
+          <div className="movies-list">
+            {movies.map(movie => (
+              <MovieCard title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+            ))}
+          </div>
+        </main>
+      </div>
+  )
 }
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,25 @@
+import { useMovies } from "../MoviesContext"
+import { Button } from "./Button";
+
 export function SideBar() {
+  const {genres, handleClickButton, selectedGenreId} = useMovies();
   // Complete aqui
+  return (
+    <nav className="sidebar">
+        <span>Watch<p>Me</p></span>
+
+        <div className="buttons-container">
+          {genres.map(genre => (
+            <Button
+              id={String(genre.id)}
+              title={genre.title}
+              iconName={genre.name}
+              onClick={() => handleClickButton(genre.id)}
+              selected={selectedGenreId === genre.id}
+            />
+          ))}
+        </div>
+
+      </nav>
+  )
 }
